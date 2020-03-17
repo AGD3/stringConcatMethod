23c84fdd4afe426318387472a434192f24197864
AGD3 <49073762+AGD3@users.noreply.github.com>
2020-03-16 17:59:01 -0700

    initial push

---
+++ JS/scripts.js
@@ -0,0 +1,16 @@
+let str1 = "good morning";
+let str2 = "sonic";
+let combined = greeting + name;
+let something = "!!!!!";
+
+let res = combined.concat(something);
+
+const name = prompt("Hello Sonic Hedgehog");
+
+function yourName(userName) {
+    return (` ${"Hello"} ${userName} `);
+}
+
+let result = yourName(Sonic);
+
+console.log(`${"Hello"} ${"Sonic Hedgehog"}`);
